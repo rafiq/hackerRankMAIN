57ead5bbfd9a2e00ba43a2dc30f455f3aaf1124f
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-10-16 19:58:40 +0300

    Create matchingSpecificCharREGEX.js

---
+++ oct2021/matchingSpecificCharREGEX.js
@@ -0,0 +1,23 @@
+/**
+ *
+ *
+ * length = 6
+ * first char = 1,2,3
+ * second char=1,2,0
+ * third     = x,s,0
+ * fourth    = 3,0,A,a
+ * fifth      =x,s,u
+ * sixth     = . ,
+ *
+ */
+
+
+/^[123][120][sx0][30Aa][xsu][\.,]$/
+
+/^[^\d][^aeiou][^bcDF][^\s][^AEIOU][^\.,]$/
+
+/^[a-z][1-9][^a-z][^A-Z][A-Z]+/
+
+/^[1-9][0-9]+[a-z]*[A-Z]*$/
+
+/^[\d]+[A-Z]+[a-z]+$/
