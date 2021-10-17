2c4388004c3b5473d2d0c64ff4eb5e4b72969b58
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-10-17 19:49:04 +0300

    Create getArea.js

---
+++ oct2021/getArea.js
@@ -0,0 +1,11 @@
+function getArea(length, width) {
+    let area;
+    let perimeter = (2 * length) + (2 * width);
+
+    area = length * width;
+
+    return area;
+}
+console.log(
+    getArea(3,4.5)
+)
