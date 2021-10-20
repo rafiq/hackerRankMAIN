0a5c5399ff0b1a47efd3384ce0bff3abc4a4dfc7
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-10-20 20:08:34 +0300

    Update triplets.js

--- oct2021/triplets.js
+++ oct2021/triplets.js
@@ -1,27 +1,15 @@
 function solve(d) {
     let count = 0;
     let set = new Set()
-    let hasAllElems = true;
 
-    for (let i = 0; i < arr1.length; i++){
-        if (arr2.indexOf(arr1[i]) === -1) {
-            hasAllElems = false;
-            break;
-        }
-    }
-
-    // let hash = {};
-// https://www.hackerrank.com/challenges/triplets/problem
     for (let i = 0; i < d.length; i++) {
-        // set a variable here that finds the next number
-        // let
+
         for (let j = i + 1; j < d.length; j++) {
             for (let k = j + 1;  k < d.length; k++) {
 
-                if (d[i] < d[j] ) {
-                    if (d[j] < d[k]) {
-                        let current = [d[i],d[j],d[k]];
-
+                if (d[i] < d[j] && d[i] !== d[j]) {
+                    if (d[j] < d[k] && d[j] !== d[k]) {
+                        let current = String(d[i]) + String(d[j]) + String(d[k]);
                         if (set.has(current)) continue;
                         set.add(current)
                         count++
@@ -30,7 +18,6 @@ function solve(d) {
             }
         }
     }
-    console.log(set)
     return count;
 
 }
