15de81ba3d67e91af9c60eb91c0e0933d5dc7eb4
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-10-23 19:46:56 +0300

    Create binaryTree.js

---
+++ oct2021/binaryTree.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor(d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+    }
+
+};
+
+Node * insert( root, data) {
+
+
+    return root;
+}
+let obj = new Node()
+
+console.log(
+    obj
+);
